f036ede46e5e442d26d0b4805ec6114d6eabbdd6
Romaric fosso <romaricfosso6@gmail.com>
2020-11-27 14:51:58 -0800

    add a projetct mvn

---
+++ timesheet/src/main/java/org/exemple/timesheet/App.java
@@ -0,0 +1,13 @@
+package org.exemple.timesheet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! welcome romaric" );
+    }
+}
